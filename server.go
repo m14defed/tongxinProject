ed91a59a8c5000770477d035adaf207db6bfd215
m14defed <143387586+m14defed@users.noreply.github.com>
2023-11-26 08:14:57 +0800

    tongxun v 0.3

--- server.go
+++ server.go
@@ -2,21 +2,75 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"sync"
 )
 
+// 创建一个服务器
 type Server struct {
-	Ip   string
-	port string
+	//服务器ip地址
+	Ip        string
+	port      string
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	Message chan string
 }
 
+// 服务器工厂
 func NewServer(ip string, port string) *Server {
-	return &Server{Ip: ip, port: port}
-}
-func (this *Server) Handler(coon net.Conn) {
-	fmt.Println("holle world")
+	return &Server{Ip: ip, port: port, OnlineMap: make(map[string]*User), Message: make(chan string)}
 }
 
+// 监听Messager实时广播消息
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		this.mapLock.RLock()
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+
+		}
+		this.mapLock.RUnlock()
+
+	}
+}
+
+// 向广播中心发送消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Username + ":" + msg + "]"
+	this.Message <- sendMsg
+}
+
+// 连接后的的处理器
+func (this *Server) Handler(conn net.Conn) {
+	fmt.Println("holle world")
+	user := NewUser(conn)
+
+	this.mapLock.Lock()
+	this.OnlineMap[user.Username] = user
+	this.mapLock.Unlock()
+	//广播上线消息
+	this.BroadCast(user, "login success")
+	//广播用户的消息
+	Duque := make([]byte, 2048)
+	n, err := conn.Read(Duque)
+	if n == 0 {
+		fmt.Println("用户下线")
+		this.BroadCast(user, "user offline")
+		return
+	}
+	if err != nil && err != io.EOF {
+		fmt.Println("conn err", err)
+	}
+	msg := string(Duque[:n-1])
+	this.BroadCast(user, msg)
+	select {}
+
+}
+
+// 启动服务器
 func (this *Server) Start() {
 	//监听
 	listen, err := net.Listen("tcp", fmt.Sprintf("%v:%v", this.Ip, this.port))
@@ -25,6 +79,8 @@ func (this *Server) Start() {
 	}
 	//关闭
 	defer listen.Close()
+	//启动消息推发
+	go this.ListenMessager()
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
@@ -32,6 +88,5 @@ func (this *Server) Start() {
 			continue
 		}
 		go this.Handler(conn)
-
 	}
 }
--- user.go
+++ user.go
@@ -6,24 +6,32 @@ import (
 )
 
 type User struct {
-	Username string      `json:"username,omitempty"`
-	Address  string      `json:"address,omitempty"`
-	C        chan string `json:"c,omitempty"`
-	Conn     net.Conn    `json:"conn,omitempty"`
+	//用户名
+	Username string `json:"username,omitempty"`
+	//地址
+	Address string `json:"address,omitempty"`
+	//channel
+	C chan string `json:"c,omitempty"`
+	//连接
+	Conn net.Conn `json:"conn,omitempty"`
 }
 
+// 用户工厂
 func NewUser(conn net.Conn) *User {
 	s := conn.RemoteAddr().String()
 	u := &User{Username: s, Address: s, C: make(chan string), Conn: conn}
 	go u.ListerMesage()
 	return u
 }
+
+// 实时从私人消息中得到消息
 func (this *User) ListerMesage() {
 	for {
 		// 阻塞，等待消息
 		msg := <-this.C
 		// 发送消息
 		this.Conn.Write([]byte(msg))
+		//服务端控制台打印
 		fmt.Println(msg)
 	}
 }
